049b0903e576404bd10a41fe9ddf304fdcf11662
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2022-09-12 19:31:59 +0100

    Add optional parameter

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['https://unsplash.com/']
+  }
 }
 
 module.exports = nextConfig
---
+++ playground.ts
@@ -0,0 +1,35 @@
+// TS recommend use interfaces over Types when possible
+// interfaces describe object in a more natural way
+//Types allows aliases 
+type Person = {
+  name: string
+  age?: number
+}
+interface PersonLoggerFn{
+  (name: string, age: number): string
+} 
+
+export default function play(){
+  const name: string = "Flip"
+  const age: number = 34
+
+  const person: Person = {
+    name: "John",
+    age: 85
+  }
+
+  const logPersonInfo: PersonLoggerFn= (personName: string, personAge:number)=>{
+    const info = `Name: ${personName} and age: ${personAge}`
+    console.log(info);
+    return info;
+  }
+
+function logPersonInfo2(person: Person){
+  const info =`Name: ${person.name} and age: ${person.age}`
+  console.log(info);
+  return info
+}
+
+  logPersonInfo2(person)
+  logPersonInfo(name, age)
+}
